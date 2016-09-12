79a60a5a5fc43b667b486e91e8567bde06fbc926
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-09-12 19:13:02 +0900

    delete useless menu on SelectUserActivity

--- app/src/main/java/jp/co/fujixerox/deviceman/SelectUserActivity.java
+++ app/src/main/java/jp/co/fujixerox/deviceman/SelectUserActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -19,13 +17,9 @@ import jp.co.fujixerox.deviceman.network.Apiclient;
 
 public class SelectUserActivity extends AppCompatActivity {
     private static final String TAG = SelectUserActivity.class.getSimpleName();
-    private static final int REQUEST_CODE_TO_SCAN_USER_ID = 9001;
-
-    private View mView;
 
     /* dependency */
     private Apiclient mApiclient;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +29,6 @@ public class SelectUserActivity extends AppCompatActivity {
         mApiclient = new Apiclient();
 
         requestAllUserInfoToSeriver();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /*******************
